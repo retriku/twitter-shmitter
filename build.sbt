8fa9fe918991591b18bc08d9aafd78c141e958b4
retriku <renars.kudins@gmail.com>
2016-03-29 20:24:08 +0300

    dependencies

--- build.sbt
+++ build.sbt
@@ -1,8 +1,19 @@
+import Dependencies._
 
 lazy val commonSettings = Seq(
-	scalaVersion := "2.11.7"
-)
+  scalaVersion := "2.11.7",
+  libraryDependencies ++= Seq(
+    scalaLogging,
+    logback,
+    scalaTest
+  ))
 
 lazy val `twitter-shmitter` = (project in file("twitter"))
-	.settings(commonSettings: _*)
+  .settings(commonSettings: _*)
+  .settings(
+    libraryDependencies ++= Seq(
+      h2,
+      slick,
+      postgres
+    ))
 
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,5 +1,13 @@
 import sbt._
 
-object Libraries {
-
+object Dependencies {
+  val scalactic = "org.scalactic" %% "scalactic" % "2.2.6"
+  val scalaTest = "org.scalatest" %% "scalatest" % "2.2.6" % "test"
+  val akka = "com.typesafe.akka" %% "akka-actor" % "2.4.2"
+  val slick = "com.typesafe.slick" %% "slick" % "3.1.1"
+  val slf4j = "org.slf4j" % "slf4j-api" % "1.7.19"
+  val logback = "ch.qos.logback" % "logback-classic" % "1.1.2"
+  val scalaLogging = "com.typesafe.scala-logging" %% "scala-logging" % "3.1.0"
+  val h2 = "com.h2database" % "h2" % "1.4.191" % "test"
+  val postgres = "org.postgresql" % "postgresql" % "9.4.1208.jre7"
 }
